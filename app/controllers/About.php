3280a582d929bf45fa0438911a40587efc1f87ab
yudialfayat <ayudi3521@gmail.com>
2021-09-16 08:47:36 +0700

    setup url and controller

---
+++ app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php 
+
+class About{
+
+    public function index($name = 'World')
+    {
+        echo "Hello $name!";
+    }
+
+    public function page()
+    {
+        echo 'about/page';
+    }
+
+}
