fe0f72a5bec17f0c659637f6733494c89e18ed00
yudialfayat <ayudi3521@gmail.com>
2021-09-16 10:14:52 +0700

    use the templates

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -4,13 +4,21 @@ class About extends Controller{
 
     public function index($name = 'World')
     {
-        $this->view('about/index');
+        $data['title'] = 'About';
+
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer', $data);
     }
 
     public function page($name = 'Hello, World!')
     {
+        $data['title'] = 'Pages | About';
         $data['name'] = $name;
+
+        $this->view('templates/header', $data);
         $this->view('about/page', $data);
+        $this->view('templates/footer', $data);
     }
 
 }
--- app/views/about/page.php
+++ app/views/about/page.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page | About</title>
-</head>
-<body>
-    <h1>Page <?php echo $data['name']; ?></h1>
-</body>
-</html>
+<h1>Page <?php echo $data['name']; ?></h1>
