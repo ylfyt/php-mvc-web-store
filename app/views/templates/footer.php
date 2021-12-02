c0e1d5f45c8f81ad269fe0023351225ec3c08091
yudialfayat <ayudi3521@gmail.com>
2021-12-02 18:02:38 +0700

    Remove bootstrap

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,5 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="<?= BASEURL ?>/public/js/bootstrap.bundle.min.js"></script>
 <script src="<?= BASEURL ?>/public/js/script.js"></script>
 
 </body>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data['title'] ?></title>
 
-    <link href="<?= BASEURL ?>/public/css/bootstrap.css" rel="stylesheet">
-
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
